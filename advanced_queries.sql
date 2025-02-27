f416a3f7747724b20433881009b6aaa5c45c121c
Manya Singh <114657669+Manya-singh2001@users.noreply.github.com>
2025-02-27 20:47:56 +0530

    Update advanced_queries.sql

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -17,3 +17,29 @@ Display the member's_id, member's name, book title, issue date, and days overdue
 -- issued_status == members == books == return_status
 -- filter books which is return
 -- overdue > 30 
+
+SELECT 
+    ist.issued_member_id,
+    m.member_name,
+    bk.book_title,
+    ist.issued_date,
+    DATEDIFF(CURRENT_DATE, ist.issued_date) AS overdue_days
+FROM issued_status AS ist
+JOIN members AS m 
+    ON m.member_id = ist.issued_member_id 
+JOIN books AS bk 
+    ON bk.isbn = ist.issued_book_isbn
+LEFT JOIN return_status AS rs
+    ON rs.issued_id = ist.issued_id 
+WHERE 
+    (rs.return_date IS NULL OR rs.return_date > CURRENT_DATE)  -- Ensures overdue books are counted
+    AND DATEDIFF(CURRENT_DATE, ist.issued_date) > 30
+    ORDER BY 1 ;
+
+
+
+-- 
+/*    
+Task 14: Update Book Status on Return
+Write a query to update the status of books in the books table to "Yes" when they are returned (based on entries in the return_status table).
+*/
