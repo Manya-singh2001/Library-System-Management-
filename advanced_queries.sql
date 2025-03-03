dcb78d4f966c01ef0b7c232cd56440665ac6e06a
Manya Singh <114657669+Manya-singh2001@users.noreply.github.com>
2025-03-03 18:28:21 +0530

    Update advanced_queries.sql

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -158,5 +158,22 @@ SELECT * FROM branch_reports;
 -- Task 16: CTAS: Create a Table of Active Members
 -- Use the CREATE TABLE AS (CTAS) statement to create a new table active_members containing members who have issued at least one book in the last 2 months.
 
+CREATE TABLE active_members
+AS
+SELECT * FROM members
+WHERE member_id IN (SELECT 
+                        DISTINCT issued_member_id   
+                    FROM issued_status
+                    WHERE 
+                        issued_date >= CURRENT_DATE - INTERVAL '2 month'
+                    )
+;
+
+
+SELECT * FROM active_members;
+
+-- 
+-- Task 17: Find Employees with the Most Book Issues Processed
+-- Write a query to find the top 3 employees who have processed the most book issues. Display the employee name, number of books processed, and their branch.
 
 
